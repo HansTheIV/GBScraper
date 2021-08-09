a22b30678aeb2086e6e3468d121b8f63f8f3bc4e
Felix Zhang <felixfzhang@hotmail.com>
2021-08-09 10:02:07 -0700

    Added additional error information

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,8 @@
+import sys
 import unittest
+
+# allows import to 'see' files in the base directory
+sys.path.append('../')
 
 from main import getType
 
@@ -8,14 +12,15 @@ class ScraperTests(unittest.TestCase):
             ('[GB] GMK Trüffelschwein // August 5 - September 6, 2021', 0),
             ('[GB] KAT Overgrown // August 5 - September 5, 2021', 0),
             ('[GB] ePBT Acid House and Sweet Girl // August 5 - September 5', 0),
-            ('[In-Stock] DROP + MARVEL MT3 Black Panther // August 2, 2021', 0)
+            ('[In-Stock] DROP + MARVEL MT3 Black Panther // August 2, 2021', 0),
             ('[GB] Harimau & Penyu Switch // August 8 - August 31, 2021', 1),
             ('[GB] Latrialum Deskmat // August 6 - August 20, 2021', 2),
             ('[GB] Freebird60 // August 2 - September 2, 2021', 3),
             ('[GB] LW-67 // July 31 - August 31, 2021', 3),
-            ('[GB] Alter Keyboard // July 21 - August 2, 2021', 3)
-            ('[GB] Work Louder // August 5, 2021 - 100 units', 4)
+            ('[GB] Alter Keyboard // July 21 - August 2, 2021', 3),
+            ('[GB] Work Louder // August 5, 2021 - 100 units', 4),
         )
         for title, output in tests:
             with self.subTest():
-                self.assertEqual(getType(title), output)
+                self.assertEqual(getType(title), output, msg=f'{title}')
+
